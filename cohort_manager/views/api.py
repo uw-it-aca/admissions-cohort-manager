05773c7dadb4814e30452b9d0dcf51ae8a8c00b4
Jim Laney <jlaney@uw.edu>
2019-08-26 10:03:32 -0700

    updated upload view to use model

--- cohort_manager/views/api.py
+++ cohort_manager/views/api.py
@@ -1,14 +1,29 @@
-import json
-
 from django.http import HttpResponse
 from django.views import View
+from cohort_manager.models import AssignmentImport
+import json
 
 
-class UploadView(View):
-    def post(self, request):
+class RESTDispatch(View):
+    @staticmethod
+    def json_response(content='', status=200):
+        return HttpResponse(json.dumps(content, sort_keys=True),
+                            status=status,
+                            content_type='application/json')
+
+
+class UploadView(RESTDispatch):
+    def post(self, request, *args, **kwargs):
         upload_file = request.FILES['file']
-        upload_text = ""
-        for line in upload_file:
-            upload_text = upload_text + line.decode()
-        return HttpResponse(upload_text)
 
+        # TODO: validation
+        status = 200
+
+        assignment_import = AssignmentImport(
+            status_code=status,
+            document=upload_file.read().decode('utf-8'),
+            imported_by='TODO')
+        assignment_import.save()
+
+        return self.json_response(
+            status=status, content=assignment_import.json_data())
