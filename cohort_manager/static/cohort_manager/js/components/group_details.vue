828ec6edceefba61198083ec792989d0aab7ddb3
Jason Civjan <jcivjan@uw.edu>
2019-09-10 11:28:10 -0700

    mobile friendly group details component

--- cohort_manager/static/cohort_manager/js/components/group_details.vue
+++ cohort_manager/static/cohort_manager/js/components/group_details.vue
@@ -1,16 +1,16 @@
 <template>
   <b-container class="aat-details-container aat-form-section" fluid>
     <b-row>
-      <b-col cols="12" md="4" class="aat-group-info-primary">
-        <b-row class="aat-info-spacing"><div>Cohort</div> <div class="aat-group-data">25</div></b-row>
-        <b-row>Applications Assigned <span class="aat-group-data">412</span></b-row>
+      <b-col cols="12" md="3" class="aat-group-info-primary">
+        <b-row class="aat-info-spacing"><div class="aat-data-primary">Cohort</div> <div class="aat-group-data aat-data-primary">25</div></b-row>
+        <b-row><div class="aat-data-primary">Applications Assigned</div> <div class="aat-group-data aat-data-primary">412</div></b-row>
       </b-col>
       <b-col cols="12" md="4" class="aat-group-info-secondary">
         <b-row class="aat-info-spacing">Residency <span class="aat-group-data">wa-res</span></b-row>
         <b-row class="aat-info-spacing">Admit Decision<span class="aat-group-data">Admit</span></b-row>
         <b-row>Protected Group <span class="aat-group-data">No</span></b-row>
       </b-col>
-      <b-col cols="12" md="4" class="aat-group-info-secondary">
+      <b-col cols="12" md="5" class="aat-group-info-secondary">
         <b-row>Decision<span class="aat-group-data">WA Resident general admits</span></b-row>
       </b-col>
     </b-row>
@@ -43,16 +43,15 @@
     &.aat-details-container {
       border: solid #777 1px;
       color: #777;
-      font-size: 1rem;
-      line-height: 1;
+      font-size: 0.85rem;
+      line-height: 1.5;
       margin: 1.5rem 0 0;
       max-width: 800px;
       padding: 2rem;
     }
 
     .row {
-      align-items: center;
-      justify-content: center;
+      margin: 0;
     }
   }
 
@@ -71,9 +70,7 @@
     .row {
       align-items: unset;
       flex-direction: column;
-      font-size: 0.85rem;
       font-weight: normal;
-      line-height: 1.5;
       margin: 0 auto;
       padding: 0 25%;
     }
@@ -93,8 +90,26 @@
     font-weight: bold;
   }
 
+  .aat-data-primary {
+    margin: auto;
+    text-align: center;
+  }
+
   .aat-info-spacing {
-    padding-bottom: 2.5rem;
+    padding-bottom: 1.5rem;
+  }
+
+  @media screen and (max-width: 767px) {
+    .aat-group-info-primary {
+      margin-bottom: 2rem;
+      text-align: center;
+    }
+
+    .aat-group-info-secondary {
+      margin-bottom: 1rem;
+      text-align: center;
+    }
+
   }
 
 </style>
