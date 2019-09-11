c5bbff5376a4664ceaaf5272071ae01beecf499b
Jason Civjan <jcivjan@uw.edu>
2019-09-11 16:50:09 -0700

    adding draft of cohort/major table

--- cohort_manager/static/cohort_manager/js/components/collection_list.vue
+++ cohort_manager/static/cohort_manager/js/components/collection_list.vue
@@ -1,10 +1,41 @@
 <template>
   <div>
     <h2>Collection List {{ collectionType }}</h2>
-    <div v-if="collectionType === 'Cohort'">You, Cohort, you!</div>
-    <div v-else-if="collectionType === 'Major'">You, Major, you!</div>
-    <div v-else>Error: There was an issue with your request. Please select a link from the left column to try again.</div>
+    <div v-if="collectionType === 'Cohort'">
+        You, Cohort, you!
+        <b-table hover :items="cohorts" :fields="cohortFields">
+            <template v-slot:cell(actions)="row">
+                <b-button size="sm" @click="info(row.item, row.index, $event.target)" class="mr-1">
+                Info modal
+                </b-button>
+                
+            </template>
+
+            <template v-slot:row-details="row">
+                <b-card>
+                <ul>
+                    <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value }}</li>
+                </ul>
+                </b-card>
+            </template>
+        </b-table>
+
+    <!-- Info modal -->
+        <b-modal :id="infoModal.id" :title="infoModal.title" ok-only @hide="resetInfoModal">
+        <pre>{{ infoModal.content }}</pre>
+        </b-modal>
+    </div>
+
+    <div v-else-if="collectionType === 'Major'">
+        You, Major, you!
+        <b-table hover :items="majors" :fields="majorFields"></b-table>
+        </div>
+
+    <div v-else>
+        Error: There was an issue with your request. Please select a link from the left column to try again.
+        </div>
   </div>
+
 </template>
 
 <script>
@@ -20,13 +51,91 @@
     },
     data(){
       return {
-        
+        cohortFields: [
+          {
+            key: 'cohort_#',
+            sortable: true
+          },
+          {
+            key: 'Description',
+            sortable: true
+          },
+          {
+            key: 'Residency',
+            sortable: true,
+          },
+          {
+            key: 'Protected',
+            sortable: true
+          },
+          {
+            key: 'Admit_Status',
+            sortable: true
+          },
+          {
+            key: 'Assigned',
+            sortable: true,
+          },
+          { key: 'actions', label: 'Actions' }
+        ],
+        cohorts: [
+          { 'cohort_#': '1', Description: 'TEST: Residents, admit', Residency: 'WA-residents', Protected: 'No', Admit_Status: 'Admit', Assigned: '120'  },
+          { 'cohort_#': '2', Description: 'TEST: Non-Residents, admit', Residency: 'WA-residents', Protected: 'No', Admit_Status: 'Admit', Assigned: '32'  },
+          { 'cohort_#': '3', Description: 'TEST: Protected, Soccer', Residency: 'WA-residents', Protected: 'Yes', Admit_Status: 'Admit', Assigned: '0'  },
+          { 'cohort_#': '99', Description: 'TEST: Lost Souls, deny', Residency: 'WA-residents', Protected: 'No', Admit_Status: 'Deny', Assigned: '1'  }
+        ],
+        majorFields: [
+          {
+            key: 'Major',
+            sortable: true
+          },
+          {
+            key: 'Division',
+            sortable: true
+          },
+          {
+            key: 'College',
+            sortable: true,
+          },
+          {
+            key: 'DTX',
+            sortable: true
+          },
+          {
+            key: 'Assigned',
+            sortable: true
+          },
+          {
+            key: 'Actions',
+            label: '',
+            sortable: false,
+          }
+        ],
+        majors: [
+          { Major: 'TEST: American ethnic studies', Division: 'Humanities', College: 'Arts and Sciences', DTX: 'No', Assigned: '0' },
+          { Major: 'Anthropology', Division: 'Humanities', College: 'Arts and Sciences', DTX: 'No', Assigned: '134' },
+          { Major: 'Aquatic & fishery sciences', Division: 'Natural Sciences', College: 'Arts and Sciences', DTX: 'No', Assigned: '12' },
+          { Major: 'Engineering undeclared', Division: 'Humanities', College: 'College of Engineering', DTX: 'Yes', Assigned: '322' }
+        ],
+        infoModal: {
+          id: 'info-modal',
+          title: '',
+          content: ''
+        }
       };
     },
     mounted() {
-
     },
     methods: {
+        info(item, index, button) {
+        this.infoModal.title = `Row index: ${index}`
+        this.infoModal.content = JSON.stringify(item, null, 2)
+        this.$root.$emit('bv::show::modal', this.infoModal.id, button)
+      },
+      resetInfoModal() {
+        this.infoModal.title = ''
+        this.infoModal.content = ''
+      },
     }
   };
 </script>
