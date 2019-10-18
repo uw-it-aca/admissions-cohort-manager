44c651dd0a686e1ff3d7055741ab63d6efbd6689
Jason Civjan <jcivjan@uw.edu>
2019-10-18 16:27:43 -0700

    removing unused prop

--- cohort_manager/static/cohort_manager/js/components/collection_upload_file_input.vue
+++ cohort_manager/static/cohort_manager/js/components/collection_upload_file_input.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-form-file v-model="file" :state="Boolean(file)" placeholder="Choose a file or drop it here..." drop-placeholder="Drop file here..." id="file" class="aat-file-input" @change="selectedFile">
+  <b-form-file :state="Boolean(file)" placeholder="Choose a file or drop it here..." drop-placeholder="Drop file here..." id="file" ref="file" class="aat-file-input" @change="selectedFile">
   </b-form-file>
 </template>
 
