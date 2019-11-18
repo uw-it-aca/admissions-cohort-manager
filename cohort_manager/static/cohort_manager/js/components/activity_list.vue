a6f363c6221de51c8f7d685b6924cd69c8ace09b
Stephen De Vight <devights@gmail.com>
2019-11-18 14:43:26 -0800

    fixing pagination

--- cohort_manager/static/cohort_manager/js/components/activity_list.vue
+++ cohort_manager/static/cohort_manager/js/components/activity_list.vue
@@ -15,7 +15,7 @@
 
     <b-row>
       <b-col cols="12" md="10" lg="9" order-lg="1" order="2" class="aat-col-nopad aat-activity-table">
-        <b-table 
+        <b-table
           id="assignment_history_table"
           hover
           responsive
@@ -231,12 +231,13 @@
         ).then(response => {
           if(response.status === 200){
             this.activities = response.data.activities;
+            this.totalRows = this.activities.length;
           }
         });
       },
       selectCollection(id){
         var id_to_set;
-        if(isNaN(id)){
+        if(isNaN(id) && id !== undefined){
           $(this.majorOptions).each(function(idx, option){
             if (id.toLowerCase() === option.value.toLowerCase()){
               id_to_set = id;
