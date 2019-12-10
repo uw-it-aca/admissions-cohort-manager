642f5944be72590c57923e0afd2b8f24a52d260b
Jason Civjan <jcivjan@uw.edu>
2019-12-09 16:00:27 -0800

    remove filter links from cohort/major lists

--- cohort_manager/static/cohort_manager/js/components/collection_list.vue
+++ cohort_manager/static/cohort_manager/js/components/collection_list.vue
@@ -11,7 +11,6 @@
         :fields="cohortFields"
       >
         <template v-slot:cell(actions)="row">
-          <a :href="'/log/' + row.item.id" :title="'Activity for cohort ' + row.item.name"><i class="far fa-clock" /><span class="sr-only">Activity</span></a>
           <a :href="'http://www.tableau.com/uw/cohort/' + row.item.id" :title="'View cohort ' + row.item.name + ' in Tableau'"><i class="fas fa-external-link-alt" /><span class="sr-only">View cohort in Tableau</span></a>
           <a :href="'/cohort/' + row.item.id" :title="'Assign applications to cohort ' + row.item.name">Assign</a>
           <b-button size="sm" :title="'Remove all assignments to cohort ' + row.item.name" @click="info(row.item, row.index, $event.target)">
@@ -32,7 +31,6 @@
         :fields="majorFields"
       >
         <template v-slot:cell(actions)="row">
-          <a :href="'/log/' + row.item.id" :title="'Activity for major ' + row.item.name"><i class="far fa-clock" /><span class="sr-only">Activity</span></a>
           <a :href="'/major/' + row.item.id" :title="'Assign applications to major ' + row.item.name">Assign</a>
           <b-button size="sm" :title="'Remove all assignments to major' + row.item.name" @click="info(row.item, row.index, $event.target)">
             Reset
