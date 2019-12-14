7b0da2ca02b8903c184e711933f4c75fdba5f3cb
Stephen De Vight <devights@gmail.com>
2019-12-13 16:30:18 -0800

    adding loginrequired

--- cohort_manager/views/pages.py
+++ cohort_manager/views/pages.py
@@ -1,12 +1,11 @@
 from django.conf import settings
 from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from uw_saml.decorators import group_required
 from uw_saml.utils import get_user
 
 
-@method_decorator(group_required(settings.ALLOWED_USERS_GROUP),
-                  name='dispatch')
 class PageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -14,5 +13,9 @@ class PageView(TemplateView):
         return context
 
 
+@method_decorator(login_required(),
+                  name='dispatch')
+@method_decorator(group_required(settings.ALLOWED_USERS_GROUP),
+                  name='dispatch')
 class LandingView(PageView):
     template_name = "landing.html"
