24e34fdd5738a863006b213c3d0904bc02d87596
Jason Civjan <jasoncivjan@Unknown.domain>
2019-12-26 09:02:09 -0800

    updating crm link

--- cohort_manager/static/cohort_manager/js/App.vue
+++ cohort_manager/static/cohort_manager/js/App.vue
@@ -71,7 +71,7 @@
               <b-link href="https://bitools.uw.edu/#/site/Transitional/projects/575" tabindex="1" class="nav-link aat-link aat-external-link" target="_blank">
                 Tableau Selection Tool
               </b-link>
-              <b-link href="https://crm.recruit.uw.edu/" tabindex="1" class="nav-link aat-link" target="_blank">
+              <b-link href="https://crm.recruit.uw.edu/Seattle" tabindex="1" class="nav-link aat-link" target="_blank">
                 CRM Recruiter
               </b-link>
             </li>
