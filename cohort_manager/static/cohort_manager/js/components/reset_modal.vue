9836d214f1fe28324fc62110be5bbca45debd522
Stephen De Vight <devights@uw.edu>
2020-10-19 17:27:14 -0700

    wiring up pg reset modal

--- cohort_manager/static/cohort_manager/js/components/reset_modal.vue
+++ cohort_manager/static/cohort_manager/js/components/reset_modal.vue
@@ -111,18 +111,20 @@
         this.csrfToken = $cookies.get("csrftoken");
       },
       handleReset(bvModalEvent) {
-        var vue = this;
+        var vue = this,
+            url = '/api/collection/'
+              + this.collectionType.toLowerCase()
+              + "/"
+              + this.current_period
+              + "/";
         // disable submit after click
         bvModalEvent.preventDefault();
         this.is_resetting = true;
+        if(this.collectionType !== "purplegold"){
+          url += this.itemId + "/";
+        }
         axios.delete(
-          '/api/collection/'
-            + this.collectionType.toLowerCase()
-            + "/"
-            + this.current_period
-            + "/"
-            + this.itemId
-            + "/",
+          url,
           {
             headers: {
               'Content-Type': 'multipart/form-data',
