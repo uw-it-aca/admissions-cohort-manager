d77553594a75ec00af8c82d86000905f2d6de6ad
Stephen De Vight <devights@uw.edu>
2020-12-07 15:37:52 -0800

    hide reassign protected

--- cohort_manager/static/cohort_manager/js/components/collection_upload_review.vue
+++ cohort_manager/static/cohort_manager/js/components/collection_upload_review.vue
@@ -65,7 +65,7 @@
             Note: Applications with a protected cohort will not be reassigned.
           </b-form-text>
         </span>
-        <span v-else-if="has_protected" id="reassign_collection_protected">
+        <span v-else-if="has_protected && uploadType !== 'bulk'" id="reassign_collection_protected">
           <b-form-checkbox
             id="app_unprotect_checkbox"
             v-model="is_reassign_protected"
