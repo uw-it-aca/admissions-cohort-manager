2e70f1dbe759d8b0f2eb296657855480f9d42bae
Stephen De Vight <devights@uw.edu>
2021-01-07 14:47:27 -0800

    load nvm

--- travis-ci/test.sh
+++ travis-ci/test.sh
@@ -28,6 +28,10 @@ function run_test {
 run_test "pycodestyle ${DJANGO_APP}/ --exclude='migrations,resources,static'"
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 nvm install 14.15
 nvm use node
 node -v
