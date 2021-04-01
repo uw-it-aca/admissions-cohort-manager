743936e33a6e9b525de7bf710b8b1a46b000ccf8
Jim Laney <jlaney@uw.edu>
2021-04-01 10:03:06 -0700

    adds license/copyright info to src files

--- cohort_manager/tests/test_models.py
+++ cohort_manager/tests/test_models.py
@@ -1,3 +1,6 @@
+# Copyright 2021 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
 from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
 from cohort_manager.models import AssignmentImport, Assignment
